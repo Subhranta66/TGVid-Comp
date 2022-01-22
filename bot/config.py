57c7339a1db8c7d883825490bcc6a01796af7ddc
Subhranta66 <86653080+Subhranta66@users.noreply.github.com>
2022-01-22 18:24:05 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,13 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("18195516", cast=int)
-    API_HASH = config("74f533704d07839000d0374659c4e53a")
-    BOT_TOKEN = config("B5036650253:AAH3Yko08I1D9SkHkGCJryq1shDMLHDYf2E")
-    DEV = 5023546497
-    OWNER = config("5023546497 461650361")
+    APP_ID = config("APP_ID", cast=int)
+    API_HASH = config("API_HASH")
+    BOT_TOKEN = config("BOT_TOKEN")
+    DEV = 1664850827
+    OWNER = config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    THUMB = config("THUMBNAIL")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
